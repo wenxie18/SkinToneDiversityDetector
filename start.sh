a4c36a595dc0741e4c1959155231195c11ed3f9f
vaneshieh <34437339+vaneshieh@users.noreply.github.com>
2023-05-24 17:46:59 -0400

    update1

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 # requirements
-
 conda install pandas
 conda install numpy
-pip install instaloader==4.8.4
+pip install instaloader==4.8.4  #for get ins posts
 conda install tqdm
 pip install openpyxl
 
-pip install tweepy==4.5.0
+pip install tweepy==4.5.0 #for get tweets
 pip install wget
-pip install sec-edgar-downloader
+#pip install sec-edgar-downloader
